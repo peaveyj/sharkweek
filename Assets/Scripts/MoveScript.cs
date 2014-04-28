c1bcdb2e0da1cc3570fcba6357d4b20e11eefac6
tfendall <troy.fendall@gmail.com>
2014-04-27 19:43:54 -0700

    add ducky and angel fish

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -15,13 +15,28 @@ public class MoveScript : MonoBehaviour {
 	public Vector2 direction = new Vector2(-1, 0);
 	
 	private Vector2 movement;
-	
+	private Sprite sprite;
+
+	void Awake() {
+		sprite = GetComponent<SpriteRenderer>().sprite;
+	}
+
 	void Update()
 	{
 		// 2 - Movement
 		movement = new Vector2(
 			speed.x * direction.x,
 			speed.y * direction.y);
+
+		/*
+		if (direction.x < 0 && transform.position.x < 0) {
+			Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+			bool isVisible = GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+			if (!isVisible) {
+				Destroy(gameObject, 0.5f);
+			}
+		}
+		*/
 	}
 	
 	void FixedUpdate()
