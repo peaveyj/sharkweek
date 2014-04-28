a62f76f2c4dbc6ff1fe04036f6d5528091b721ef
peaveyj <peaveyj@gmail.com>
2014-04-27 22:07:58 -0700

    good gen

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -3,32 +3,77 @@ using System.Collections;
 
 public class ObjectGenerator : MonoBehaviour {
 
-	public EnemyHealthScript enemyPrefab;
-	public float spawnDinnerTime = 3.0f;
-	private float spawnDinnerCountdown = 0.0f;
+	public EnemyHealthScript angelFishPrefab;
+	public EnemyHealthScript blueFishPrefab;
+	public EnemyHealthScript redFishPrefab;
+	public EnemyHealthScript duckPrefab;
+	//public EnemyHealthScript Prefab;
+	//public EnemyHealthScript Prefab;
+
+	public Vector2 spawnAngelFishTime = new Vector2 (3.0f, 8.0f);
+	public Vector2 spawnBlueFishTime = new Vector2 (4.0f, 8.0f);
+	public Vector2 spawnRedFishTime = new Vector2 (4.0f, 8.0f);
+	public Vector2 spawnDuckTime = new Vector2 (4.0f, 6.0f);
+
+	public float spawnAngelFishCountdown = 2.0f;
+	public float spawnBlueFishCountdown = 3.0f;
+	public float spawnRedFishCountdown = 4.0f;
+	public float spawnDuckCountdown = 5.0f;
+
 	private const float SPAWN_X = 10f;
-	private const float SPAWN_Y_RANGE = 5.5f;   //set to 0 to turn off randomness
-	private const float SPAWN_Y_OFFSET = -4.0f;   //-4 from random min point (0) is bottom of sea, 2 is on the surface
+	private const float SPAWN_UNDERWATER_Y_MIN = -4.5f;   //set to 0 to turn off randomness
+	private const float SPAWN_UNDERWATER_Y_MAX = 1.5f;   //-4 from random min point (0) is bottom of sea, 2 is on the surface
+	private const float SPAWN_SURFACE_Y_OFFSET = 2.5f;
 
 	public Transform spawner;
 
-	public EnemyHealthScript enemyInstance;
+	private EnemyHealthScript angelFishInstance;
+	private EnemyHealthScript blueFishInstance;
 	// Use this for initialization
 	void Start () {
-		enemyPrefab.CreatePool();
-
+		angelFishPrefab.CreatePool();
+		blueFishPrefab.CreatePool();
+		redFishPrefab.CreatePool();
+		duckPrefab.CreatePool();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		spawnDinnerCountdown -= Time.deltaTime;
-		if (spawnDinnerCountdown <= 0)
+		spawnAngelFishCountdown -= Time.deltaTime;
+		if (spawnAngelFishCountdown <= 0)
 		{
-			spawnDinnerCountdown = spawnDinnerTime;
+			spawnAngelFishCountdown = Random.Range(spawnAngelFishTime.x, spawnAngelFishTime.y);
 			//enemyInstance = ObjectPool.Spawn(fish_dinner, new Vector3(10, Random.Range(-3f,1.3f),0));
-			spawner.position = new Vector3(SPAWN_X, Random.value*SPAWN_Y_RANGE + SPAWN_Y_OFFSET, 0);
-			enemyInstance = enemyPrefab.Spawn(spawner.position,spawner.rotation);
+			//spawner.position = new Vector3(SPAWN_X, Random.value*SPAWN_UNDERWATER_Y_RANGE + SPAWN_UNDERWATER_Y_OFFSET, 0);
+			angelFishPrefab.Spawn(new Vector3(SPAWN_X, Random.Range(SPAWN_UNDERWATER_Y_MIN, SPAWN_UNDERWATER_Y_MAX), 0));
+		}
+
+		spawnBlueFishCountdown -= Time.deltaTime;
+		if (spawnBlueFishCountdown <= 0)
+		{
+			spawnBlueFishCountdown = Random.Range(spawnBlueFishTime.x, spawnBlueFishTime.y);
+			//enemyInstance = ObjectPool.Spawn(fish_dinner, new Vector3(10, Random.Range(-3f,1.3f),0));
+			//spawner.position = new Vector3(SPAWN_X, Random.value*SPAWN_UNDERWATER_Y_RANGE + SPAWN_UNDERWATER_Y_OFFSET, 0);
+			blueFishPrefab.Spawn(new Vector3(SPAWN_X, Random.Range(SPAWN_UNDERWATER_Y_MIN, SPAWN_UNDERWATER_Y_MAX), 0));
+		}
+
+		spawnRedFishCountdown -= Time.deltaTime;
+		if (spawnRedFishCountdown <= 0)
+		{
+			spawnRedFishCountdown = Random.Range(spawnRedFishTime.x, spawnRedFishTime.y);
+			//enemyInstance = ObjectPool.Spawn(fish_dinner, new Vector3(10, Random.Range(-3f,1.3f),0));
+			//spawner.position = new Vector3(SPAWN_X, Random.value*SPAWN_UNDERWATER_Y_RANGE + SPAWN_UNDERWATER_Y_OFFSET, 0);
+			redFishPrefab.Spawn(new Vector3(SPAWN_X, Random.Range(SPAWN_UNDERWATER_Y_MIN, SPAWN_UNDERWATER_Y_MAX), 0));
+		}
+
+		spawnDuckCountdown -= Time.deltaTime;
+		if (spawnDuckCountdown <= 0)
+		{
+			spawnDuckCountdown = Random.Range(spawnDuckTime.x, spawnDuckTime.y);
+			//enemyInstance = ObjectPool.Spawn(fish_dinner, new Vector3(10, Random.Range(-3f,1.3f),0));
+			//spawner.position = new Vector3(SPAWN_X, Random.value*SPAWN_UNDERWATER_Y_RANGE + SPAWN_UNDERWATER_Y_OFFSET, 0);
+			duckPrefab.Spawn(new Vector3(SPAWN_X, SPAWN_SURFACE_Y_OFFSET, 0));
 		}
 	
 	}
